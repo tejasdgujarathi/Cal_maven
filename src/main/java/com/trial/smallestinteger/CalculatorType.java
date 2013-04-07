2cb98692cae5c308aa454ebbf0cbcf7e58e80d88
Tejas <tgujarathi@equalexpets.com>
2013-04-07 16:42:47 +0100

    initial commit

---
+++ src/main/java/com/trial/smallestinteger/CalculatorType.java
@@ -0,0 +1,6 @@
+package com.trial.smallestinteger;
+
+
+public interface CalculatorType {
+	boolean isValid(int number) throws IllegalArgumentException;
+}
---
+++ src/main/java/com/trial/smallestinteger/MultipleOfFiveCalculator.java
@@ -0,0 +1,15 @@
+package com.trial.smallestinteger;
+
+
+public class MultipleOfFiveCalculator implements CalculatorType {
+
+	@Override
+	public boolean isValid(int number) throws IllegalArgumentException {
+		return isMultipleOfFive(number);
+	}
+
+	private boolean isMultipleOfFive(int number) {
+		return number%5 == 0;
+	}
+
+}
